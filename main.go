afb2974a391deff4ff368ab03c3081a6999002bf
michbogos <bogostamicheal@gmail.com>
2025-02-19 22:02:23 +0100

    Write html file

--- main.go
+++ main.go
@@ -3,22 +3,31 @@ package main
 import (
 	"io/fs"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 )
 
-func walk(s string, d fs.DirEntry, err error) error {
-	if err != nil {
-		return err
+func check_err(e error) {
+	if e != nil {
+		panic(e)
 	}
-	if !d.IsDir() {
-		println(s)
-	}
+}
+
+func generate(in_root string, s string, out_root string) error {
+	buffer, e := os.ReadFile(path.Join(in_root, s))
+	check_err(e)
+	check_err(e)
+	println(string(buffer))
+	e = os.WriteFile(path.Join(out_root, strings.Replace(s, ".md", ".html", 1)), buffer, 0775)
+	check_err(e)
 	return nil
 }
 
 func main() {
 	args := os.Args[1:]
-	input_dir := "markdown"
+	input_dir := ""
+	output_dir := ""
 	for i := 0; i < len(args); i++ {
 		if args[i] == "-i" {
 			if len(args) > i+1 {
@@ -27,8 +36,15 @@ func main() {
 		}
 		if args[i] == "-o" {
 			if len(args) > i+1 {
+				output_dir = args[i+1]
 			}
 		}
 	}
-	filepath.WalkDir(input_dir, walk)
+	filepath.WalkDir(input_dir, func(s string, d fs.DirEntry, err error) error {
+		if !d.IsDir() {
+			return generate(input_dir, d.Name(), output_dir)
+		} else {
+			return nil
+		}
+	})
 }
